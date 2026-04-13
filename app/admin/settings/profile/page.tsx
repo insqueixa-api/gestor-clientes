4c8457cb3d92deb51969f6334d87c86330573345
Marcio <insqueixa@gmail.com>
2026-04-13 13:41:18 -0300

    Aplicativos

--- app/admin/settings/profile/page.tsx
+++ app/admin/settings/profile/page.tsx
@@ -2441,7 +2441,7 @@ function WhatsAppSession2Panel({
   const [waSessionLabelEditing, setWaSessionLabelEditing] = useState(false);
   const [waRejectCalls, setWaRejectCalls]   = useState(true);
   const [waRejectMessage, setWaRejectMessage] = useState(
-    "Olá! Não recebo ligações pelo WhatsApp. Por favor, envie uma mensagem e aguarde meu retorno. Obrigado! 😊"
+    `{saudacao}!\nNo momento não estou recebendo ligações. Por favor, envie mensagem e aguarde retorno.\nObrigado!\n{data} {hora}.`
   );
   const [waSavingConfig, setWaSavingConfig] = useState(false);
   const [waAllowedNumbers, setWaAllowedNumbers] = useState("");
