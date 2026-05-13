b3757b96bdb7a33ce968ee731d8c7bf4c69bae3c
insqueixa-api <insqueixa@gmail.com>
2026-05-13 00:01:26 -0300

    clienteID

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,8 +12,9 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
   
   // Apontando para o diretório correto que vimos no AdminShell
   // Voltando para os ícones quadrados perfeitos na raiz
-  let icon192 = "/icon-192x192.png";
-  let icon512 = "/icon-512x512.png";
+  // Apontando para dentro da pasta brand
+  let icon192 = "/brand/icon-192x192.png";
+  let icon512 = "/brand/icon-512x512.png";
 
   if (user) {
     const { data: member } = await supabase
