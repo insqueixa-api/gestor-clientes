2c9b1eb3ef87531393b395054c4845117bfd9cae
insqueixa-api <insqueixa@gmail.com>
2026-05-16 23:30:25 -0300

    Aluno page ID

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,11 +12,9 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
   
   // Apontando para o diretório correto que vimos no AdminShell
   // Voltando para os ícones quadrados perfeitos na raiz
-// Apontando para dentro da pasta brand (VERSÃO AZUL)
-  let iconAny192 = "/brand/icon-192x192blue.png";
-  let iconAny512 = "/brand/icon-512x512blue.png";
-  let iconMask192 = "/brand/icon-192x192.png";
-  let iconMask512 = "/brand/icon-512x512.png";
+  // Apontando para dentro da pasta brand
+  let icon192 = "/brand/icon-192x192blue.png";
+  let icon512 = "/brand/icon-512x512blue.png";
 
   if (user) {
     const { data: member } = await supabase
@@ -40,7 +38,8 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
         themeColor = tenant.primary_color || themeColor;
 
         if (tenant.logo_url) {
-          iconAny192 = iconAny512 = iconMask192 = iconMask512 = tenant.logo_url;
+          icon192 = tenant.logo_url;
+          icon512 = tenant.logo_url;
         }
       }
     }
@@ -57,29 +56,27 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
     theme_color: themeColor,
     icons: [
       {
-        src: iconAny192,
+        src: icon192,
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: iconAny512,
+        src: icon512,
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: iconMask192,
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
+        src: icon512,
+        sizes: "any", // Salva-vidas: Pede pro Chrome aceitar a logo da nuvem mesmo se o tamanho não bater
+        purpose: "any",
       },
       {
-        src: iconMask512,
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      }
     ],
   }
 }
